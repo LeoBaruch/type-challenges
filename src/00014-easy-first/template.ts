1356a906744b8d66f81bb5116e9ffaad5edd5d8e
gexiaolong <1013363134@qq.com>
2022-05-23 23:38:26 +0800

    feat: add 00014-easy-first

---
+++ src/00014-easy-first/template.ts
@@ -0,0 +1,11 @@
+// 1
+// type First<T extends any[]> = T extends [] ? never : T[0]
+
+// 2
+// type First<T extends any[]> = T[number] extends never ? never : T[0]
+
+// 3
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0];
+
+// 4
+type First<T extends any[]> = T extends [infer First, ... infer Other] ? First : never;
