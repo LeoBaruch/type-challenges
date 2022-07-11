cb9ce2f585e8edcd11ad1409b985207db91a6e70
gexiaolong <1013363134@qq.com>
2022-07-12 07:32:07 +0800

    feat: add 00298-medium-length-of-string

---
+++ src/00298-medium-length-of-string/template.ts
@@ -0,0 +1,8 @@
+type LengthOfString<T extends string> = StringToArray<T>['length']
+
+
+type StringToArray<T extends string> = 
+T extends '' ? [] :
+T extends `${infer S}${infer Other}` ?
+[S, ...StringToArray<Other>] :
+[]
